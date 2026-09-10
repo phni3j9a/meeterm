ad077b21db69d02215af9098d7b1b9a19fe28f37
Yuki Nakano <yvlc1.m@gmail.com>
2026-09-10 14:56:02 +0900

    Probe Keychain from an app-hosted XCTest target

--- RunnerKeychainTests.swift
+++ RunnerKeychainTests.swift
@@ -2,11 +2,11 @@ import XCTest
 import Security
 
 final class RunnerKeychainTests: XCTestCase {
-    func testRunnerKeychainAccess() {
+    func testHostKeychainAccess() {
         let account = "probe-\(UUID().uuidString)"
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
-            kSecAttrService as String: "dev.meeterm.runner-probe",
+            kSecAttrService as String: "dev.meeterm.host-probe",
             kSecAttrAccount as String: account,
             kSecAttrSynchronizable as String: false,
         ]
@@ -16,8 +16,8 @@ final class RunnerKeychainTests: XCTestCase {
 
         let addStatus = SecItemAdd(item as CFDictionary, nil)
         let deleteStatus = SecItemDelete(query as CFDictionary)
-        print("PROBE runner_keychain_add=\(addStatus)")
-        print("PROBE runner_keychain_delete=\(deleteStatus)")
+        print("PROBE host_keychain_add=\(addStatus)")
+        print("PROBE host_keychain_delete=\(deleteStatus)")
         XCTAssertEqual(addStatus, errSecSuccess)
         XCTAssertEqual(deleteStatus, errSecSuccess)
     }
